1178bd421e9829f05f05899d4319fb51b081f5fe
alexzamurca <alexzamurca@yahoo.co.uk>
2021-04-01 18:35:25 +0100

    added coloured rotating triangle. Box colliders are there but no interesting interatctions yet

---
+++ Assets/Scripts/ShapeMovement.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+public class ShapeMovement : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private bool rotateLeft = true;
+    private Rigidbody2D rb;
+    private Vector3 shapeCenter;
+
+    void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        shapeCenter = FindCenter();
+    }
+
+    private Vector3 FindCenter()
+    {
+        int i = 0;
+        Vector3 center = Vector3.zero;
+        Transform childTransform = gameObject.transform.GetChild(i);
+        while (true)
+        {
+            center += childTransform.position;
+            i++;
+            try
+            {
+                childTransform = gameObject.transform.GetChild(i);
+            }
+            catch (Exception e)
+            {
+                break;
+            }
+        }
+
+        return center / i;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 axisOfRotation = rotateLeft ? new Vector3(0, 0, 1) : new Vector3(0, 0, -1);
+        float angle = rotationSpeed * Time.deltaTime;
+        rb.transform.RotateAround(shapeCenter, axisOfRotation, angle);
+    }
+}
