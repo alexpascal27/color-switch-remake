a7f2a2ca768cf0758ddcc68942367ca07d7245ed
alexzamurca <alexzamurca@yahoo.co.uk>
2021-04-01 19:14:38 +0100

    colliders work for triangle (allowed to pass through same colour, not allowed to pass through different colour)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,24 +1,32 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private CircleCollider2D circleCollider2D;
     [SerializeField] private float jumpForce;
-    [SerializeField] private InfiniteSpawn _infiniteSpawn;
     private Vector3 currentPosition;
     private Vector3 previousPosition;
+    private List<String> colourList = new List<string>(new []{"Red", "Green", "Blue"});
+    private String colour;
 
     void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
-        //currentPosition = rb.transform.position;
-        //previousPosition = currentPosition;
+        circleCollider2D = gameObject.GetComponent<CircleCollider2D>();
+        // Pick Random colour
+        colour = colourList[Random.Range(0, colourList.Count)];
+        Debug.Log("Player is colour: " + colour);
     }
     
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
             MoveUp();
         }
@@ -27,14 +35,27 @@ public class PlayerMovement : MonoBehaviour
     private void MoveUp()
     {
         // Turn off gravity
-        float initialGravityScale = rb.gravityScale;
-        rb.gravityScale = 0f;
+        //float initialGravityScale = rb.gravityScale;
+        //rb.gravityScale = 0f;
 
         // Jump
         rb.velocity = Vector3.zero;
         rb.AddForce(new Vector2(0f, jumpForce));    
         
         // Turn gravity on
-        rb.gravityScale = initialGravityScale;
+        //rb.gravityScale = initialGravityScale;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        GameObject collisionGameObject = other.contacts[0].collider.gameObject;
+        if (collisionGameObject.CompareTag(colour))
+        {
+            Physics2D.IgnoreCollision(other.collider, circleCollider2D);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
